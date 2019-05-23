5e3d636496b17a153311221133d1737090b4b155
Rodrigo Silveira <nohopeleft13@live.com>
2019-05-23 11:52:48 -0300

    V0.5

--- app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.londonappbrewery.quizzler.models.TrueFalse;
+
 public class MainActivity extends Activity {
 
     // TODO: Declare constants here
@@ -19,22 +21,27 @@ public class MainActivity extends Activity {
     TextView mScoreTextView;
     TextView mQuestionTextView;
     ProgressBar mProgressBar;
+
+    int mIndex; //armazena a pergunta corrente
+    int mScore; //armazena a quantidade de respostas corretas
+    int mQuestion; //recebe o id da pergunta corrente
+
     // TODO: Uncomment to create question bank
-//    private TrueFalse[] mQuestionBank = new TrueFalse[] {
-//            new TrueFalse(R.string.question_1, true),
-//            new TrueFalse(R.string.question_2, true),
-//            new TrueFalse(R.string.question_3, true),
-//            new TrueFalse(R.string.question_4, true),
-//            new TrueFalse(R.string.question_5, true),
-//            new TrueFalse(R.string.question_6, false),
-//            new TrueFalse(R.string.question_7, true),
-//            new TrueFalse(R.string.question_8, false),
-//            new TrueFalse(R.string.question_9, true),
-//            new TrueFalse(R.string.question_10, true),
-//            new TrueFalse(R.string.question_11, false),
-//            new TrueFalse(R.string.question_12, false),
-//            new TrueFalse(R.string.question_13,true)
-//    };
+    private TrueFalse[] mQuestionBank = new TrueFalse[] {
+            new TrueFalse(R.string.question_1, true),
+            new TrueFalse(R.string.question_2, true),
+            new TrueFalse(R.string.question_3, true),
+            new TrueFalse(R.string.question_4, true),
+            new TrueFalse(R.string.question_5, true),
+            new TrueFalse(R.string.question_6, false),
+            new TrueFalse(R.string.question_7, true),
+            new TrueFalse(R.string.question_8, false),
+            new TrueFalse(R.string.question_9, true),
+            new TrueFalse(R.string.question_10, true),
+            new TrueFalse(R.string.question_11, false),
+            new TrueFalse(R.string.question_12, false),
+            new TrueFalse(R.string.question_13,true)
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,11 +54,27 @@ public class MainActivity extends Activity {
         mQuestionTextView = findViewById((R.id.question_text_view));
         mProgressBar = findViewById(R.id.progress_bar);
 
+        mIndex = 0;
+        mScore = 0;
+        mQuestion = mQuestionBank[mIndex].getQuestionID();
+        mQuestionTextView.setText(mQuestion);
+        mScoreTextView.setText("Score: " + mScore + "/" + mQuestionBank.length);
 
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(),"True",Toast.LENGTH_LONG).show();
+                checkAnswer(true);
+                updateQuestion();
+            }
+        });
+
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"False",Toast.LENGTH_LONG).show();
+                checkAnswer(false);
+                updateQuestion();
             }
         });
 
@@ -60,7 +83,18 @@ public class MainActivity extends Activity {
 
     }
 
-    public void clickFalse(View view) {
-        Toast.makeText(getApplicationContext(),"False",Toast.LENGTH_LONG).show();
+    public void checkAnswer(boolean answer){
+        if(answer == mQuestionBank[mIndex].isAnswer()){
+            mScore++;
+            Toast.makeText(getApplicationContext(),R.string.correct_toast,Toast.LENGTH_LONG).show();
+
+        }else {
+            Toast.makeText(getApplicationContext(), R.string.incorrect_toast, Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+    public void updateQuestion(){
+
     }
 }
